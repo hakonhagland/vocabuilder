004d00dfa164e9488c9ddb5cce0a09c6f5228221
Håkon Hægland <hakon.hagland@gmail.com>
2023-06-21 08:48:46 +0200

    Fixed datemixin test

--- tests/test_date_mixin.py
+++ tests/test_date_mixin.py
@@ -3,12 +3,13 @@ import re
 from ..vocabuilder import DateMixin
 
 class Config:
+    """Mocking the Config class in vocabuilder"""
     datefmt_str = '%Y-%m-%d %H:%M:%S'
 
-class TestDateMixin(DateMixin):
-    config = Config()
+class MyDate(DateMixin):
+    config = Config()  # used by DateMixin
 
 def test_today():
-    d = TestDateMixin()
+    d = MyDate()
     today = d.today_as_string()
-    assert re.match(r"^\d{4}", today)
+    assert re.match(r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$", today)
