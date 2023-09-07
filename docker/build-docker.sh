8fcc0d6b170b6cf3dd511f48483da538b9d0fed3
Håkon Hægland <hakon.hagland@gmail.com>
2023-09-07 20:16:14 +0200

    Fixed bug in docker build

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -10,7 +10,7 @@ dir="$dockerdir"
 fn="myapp.tar.gz"
 path="$dir"/myapp.tar.gz   # docker requires the file to be in the build context, so we put it there
 # create a tar.gz archive of the current git HEAD that respects .gitignore
-git archive -o "$fn" --format=tar.gz HEAD
+git archive -o "$path" --format=tar.gz HEAD
 # pass the archive file to docker build as a build argument
 docker build --build-arg "GIT_ARCH=$fn" -t python-vocabuilder "$dockerdir"
 rm "$path" # remove the archive file
