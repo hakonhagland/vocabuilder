59abd8b1189293514d1bc748f924e917a940992a
Håkon Hægland <hakon.hagland@gmail.com>
2023-11-02 21:33:05 +0100

    Added narrowing widgets
    Added line edits to help narrow down the number of elements in the
    database view widget

--- src/vocabuilder/view_window.py
+++ src/vocabuilder/view_window.py
@@ -1,19 +1,27 @@
 from __future__ import annotations
 
 import logging
-from typing import Callable
+from typing import Callable, Optional
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QKeyEvent
 from PyQt6.QtWidgets import (
+    QBoxLayout,
+    QHBoxLayout,
     QGridLayout,
     QLineEdit,
     QScrollArea,
+    QVBoxLayout,
     QWidget,
 )
 from vocabuilder.config import Config
 from vocabuilder.database import Database
 from vocabuilder.mixins import WarningsMixin
 from vocabuilder.widgets import QLabelClickable
+
+
+class MatchTerm:
+    TERM1: int = 1
+    TERM2: int = 2
 
 
 class ViewScrollArea(QScrollArea):
@@ -23,52 +31,75 @@ class ViewScrollArea(QScrollArea):
         items2: list[str],
         callback1: Callable[[str], None],
         callback2: Callable[[str], None],
+        fontsize: str,
     ):
         super().__init__()
-        self.items1 = items1
-        self.items2 = items2
-        self.callback1 = callback1
-        self.callback2 = callback2
+        self.items = [items1, items2]
+        self.callbacks = [callback1, callback2]
+        self.fontsize = fontsize
+        self.vbox = QVBoxLayout()
         self.scrollwidget = QWidget()
-        self.layout_ = (
-            QGridLayout()
-        )  # use layout_ to avoid name clash with QWidget.layout()
-        self.add_items()
-        self.scrollwidget.setLayout(self.layout_)
+        self.scrollwidget.setLayout(self.vbox)
+        self.add_left_and_right_column()
         self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         self.setWidgetResizable(True)
         self.setWidget(self.scrollwidget)
         return
 
-    def add_items(self) -> None:
-        vpos = 0
-        for i in range(len(self.items1)):
-            term1 = self.items1[i]
-            term2 = self.items2[i]
-            label1 = QLabelClickable(term1)
-            label1.setStyleSheet("border: 1px solid #bbbbbb;")
-            callback1 = self.item1_clicked(term1)
-            label1.addCallback(callback1)
-            self.layout_.addWidget(label1, vpos, 0)
-            label2 = QLabelClickable(term2)
-            label2.setStyleSheet("border: 1px solid #bbbbbb;")
-            callback2 = self.item2_clicked(term2)
-            label2.addCallback(callback2)
-            self.layout_.addWidget(label2, vpos, 1)
-            vpos += 1
+    def add_left_and_right_column(self) -> None:
+        self.vbox.addStretch()  # https://stackoverflow.com/a/63438161/2173773
+        self.vbox.setDirection(QBoxLayout.Direction.BottomToTop)
+        self.vbox.setSpacing(0)
+        self.vbox.setContentsMargins(0, 0, 0, 0)
+        self.add_items()
 
-    def item1_clicked(self, item1: str) -> Callable[[], None]:
-        def callback() -> None:
-            self.callback1(item1)
+    def add_items(
+        self, text: Optional[str] = None, match_term: Optional[int] = None
+    ) -> None:
+        style = f"border: 1px solid #bbbbbb; font-size: {self.fontsize}"
+        for i in reversed(range(len(self.items[0]))):
+            if (match_term is not None) and (text is not None):
+                if match_term == MatchTerm.TERM1:
+                    term1 = self.items[0][i]
+                    if not (text in term1):
+                        continue
+                elif match_term == MatchTerm.TERM2:
+                    term2 = self.items[1][i]
+                    if not (text in term2):
+                        continue
+            widget = QWidget()
+            hbox = QHBoxLayout()
+            hbox.setContentsMargins(0, 0, 0, 0)
+            widget.setLayout(hbox)
+            for j in range(0, 2):
+                term = self.items[j][i]
+                label = QLabelClickable(term)
+                label.setStyleSheet(style)
 
-        return callback
+                def callback(j: int = j, term: str = term) -> None:
+                    self.callbacks[j](term)
 
-    def item2_clicked(self, item2: str) -> Callable[[], None]:
-        def callback() -> None:
-            self.callback2(item2)
+                label.addCallback(callback)
+                hbox.addWidget(label)
+            self.vbox.addWidget(widget)
 
-        return callback
+    def update_items1(self, text: str) -> None:
+        self.update_items(text, MatchTerm.TERM1)
+
+    def update_items2(self, text: str) -> None:
+        self.update_items(text, MatchTerm.TERM2)
+
+    def update_items(self, text: str, match_term: int) -> None:
+        layout = self.vbox
+        for i in reversed(range(layout.count())):
+            layout_item = layout.itemAt(i)
+            if layout_item is not None:
+                widget = layout_item.widget()
+                if widget is not None:
+                    widget.deleteLater()
+        self.add_items(text, match_term)
+        self.scrollwidget.update()
 
 
 class ViewWindow(QWidget, WarningsMixin):
@@ -78,6 +109,7 @@ class ViewWindow(QWidget, WarningsMixin):
         self.db = database
         self.config = config
         self.window_config = self.config.config["ViewWindow"]
+        self.fontsize = self.window_config["FontSize"]
         self.setWindowTitle("View Database")
         self.resize(int(self.window_config["Width"]), int(self.window_config["Height"]))
         layout = QGridLayout()
@@ -90,9 +122,14 @@ class ViewWindow(QWidget, WarningsMixin):
     def add_line_edits(self, layout: QGridLayout, vpos: int) -> int:
         # large = self.config.config["FontSize"]["Large"]
         self.edit1 = QLineEdit(self)
+        style = f"font-size: {self.fontsize}"
+        self.edit1.setStyleSheet(style)
+        self.edit1.textChanged.connect(self.update_items1)
         # edit1.textChanged.connect(update_scroll_area_items1)
         layout.addWidget(self.edit1, vpos, 0)
         self.edit2 = QLineEdit(self)
+        self.edit2.setStyleSheet(style)
+        self.edit2.textChanged.connect(self.update_items2)
         # edit2.textChanged.connect(update_scroll_area_items2)
         layout.addWidget(self.edit2, vpos, 1)
         vpos += 1
@@ -108,7 +145,9 @@ class ViewWindow(QWidget, WarningsMixin):
         def callback2(text: str) -> None:
             self.edit2.setText(text)
 
-        self.scrollarea1 = ViewScrollArea(items1, items2, callback1, callback2)
+        self.scrollarea1 = ViewScrollArea(
+            items1, items2, callback1, callback2, self.fontsize
+        )
         layout.addWidget(self.scrollarea1, vpos, 0, 1, 2)
 
         vpos += 1
@@ -122,3 +161,9 @@ class ViewWindow(QWidget, WarningsMixin):
         if (event is not None) and event.key() == Qt.Key.Key_Escape:  # "ESC" pressed
             logging.info("ViewWindow: ESC pressed")
             self.close()
+
+    def update_items1(self) -> None:
+        self.scrollarea1.update_items1(self.edit1.text())
+
+    def update_items2(self) -> None:
+        self.scrollarea1.update_items2(self.edit2.text())
--- tests/test_view_window.py
+++ tests/test_view_window.py
@@ -1,4 +1,7 @@
 import typing
+
+import pytest
+from typing import Any, Callable
 from pytest_mock.plugin import MockerFixture
 from PyQt6.QtCore import Qt
 from vocabuilder.vocabuilder import MainWindow
@@ -16,27 +19,67 @@ class TestView:
     ) -> None:
         window = main_window
         view_win: ViewWindow = window.view_entries()
-        layout = view_win.scrollarea1.layout_
-        assert layout is not None
-        item1 = layout.itemAt(0)
+        layout1 = view_win.scrollarea1.vbox
+        assert layout1 is not None
+        # item1 = layout1.itemAt(0) # The item at index 0 is the QSpacerItem
+        item1 = layout1.itemAt(1)
         assert item1 is not None
-        term1_label = item1.widget()
+        widget1 = item1.widget()
+        assert widget1 is not None
+        layout2 = widget1.layout()
+        assert layout2 is not None
+        item2 = layout2.itemAt(0)
+        assert item2 is not None
+        term1_label = item2.widget()
         assert term1_label is not None
         term1_label = typing.cast(QLabelClickable, term1_label)
-        assert term1_label.text() == "100,000,000"
+        assert term1_label.text() == "bag"
         with qtbot.waitCallback() as callback:
             mocker.patch.object(view_win.edit1, "setText", callback)
             qtbot.mouseClick(term1_label, Qt.MouseButton.LeftButton)
         txt = callback.args[0]
-        assert txt == "100,000,000"
-        item2 = layout.itemAt(1)
-        assert item2 is not None
-        term2_label = item2.widget()
+        assert txt == "bag"
+
+        item3 = layout2.itemAt(1)
+        assert item3 is not None
+        term2_label = item3.widget()
         assert term2_label is not None
         term2_label = typing.cast(QLabelClickable, term2_label)
-        assert term2_label.text() == "억"
+        assert term2_label.text() == "가방"
         with qtbot.waitCallback() as callback:
             mocker.patch.object(view_win.edit2, "setText", callback)
             qtbot.mouseClick(term2_label, Qt.MouseButton.LeftButton)
         txt = callback.args[0]
-        assert txt == "억"
+        assert txt == "가방"
+
+    @pytest.mark.parametrize("check1, check2", [(True, False), (False, True)])
+    def test_set_text(
+        self,
+        check1: bool,
+        check2: bool,
+        main_window: MainWindow,
+        mocker: MockerFixture,
+        qtbot: QtBot,
+    ) -> None:
+        window = main_window
+        view_win: ViewWindow = window.view_entries()
+        callback_called = False
+
+        def gen_wrapper() -> Callable[[Any, Any], None]:
+            original_method = view_win.scrollarea1.add_items
+
+            def wrapper(*args: Any, **kwargs: Any) -> None:
+                nonlocal callback_called
+                original_method(*args, **kwargs)
+                callback_called = True
+
+            return wrapper
+
+        wrapper = gen_wrapper()
+        mocker.patch.object(view_win.scrollarea1, "add_items", wrapper)
+        if check1:
+            view_win.edit1.setText("bag")
+        if check2:
+            view_win.edit2.setText("가방")
+        qtbot.waitUntil(lambda: callback_called)
+        assert True
